4d9f3ad63e492a1dc7b18f8fe5d658e5cd4467fa
Tom Harrop <twharrop@gmail.com>
2019-07-29 09:59:17 +1200

    genemark download

---
+++ src/genemark_download.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+wget \
+        --post-data='program=gmet&os=linux64&name=https://bit.ly/2GAQMGz&institution=na&country=na&email=na@na.com&submit=I+agree+to+the+terms+of+this+license+agreement' \
+        http://exon.gatech.edu/GeneMark/license_download.cgi
+    bin_url="$(grep -Po 'http://.*?linux_64.tar.gz' license_download.cgi)"
+    url32="$(grep -Po "http://.*?gm_key_32.gz" license_download.cgi)"
+    url64="${url32/gm_key_32/gm_key_64}"
+    wget -O "genemark.tar.gz" "${bin_url}"
+    mkdir genemark
+    tar -zxf genemark.tar.gz \
+        -C genemark \
+        --strip-components 1
+    rm -f genemark.tar.gz
+    cd genemark || exit 1
+    rm gm_key*
+    wget "${url32}"
+    wget "${url64}"
+    gunzip *.gz
+    cd .. || exit 1
+    rm license_download.cgi
